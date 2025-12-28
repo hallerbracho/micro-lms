11ed1550b4ee846282bea58e69757afe561f95f0
Haller Bracho <habracho@fec.luz.edu.ve>
2025-12-28 10:22:56 -0400

    Add files via upload

--- Plantilla-IA.py
+++ Plantilla-IA.py
@@ -2,7 +2,9 @@
 # Variables inyectadas: st, pd, np, random, db, EXAM_ID, student_id (si ya fue ingresado)
 
 # 1. VALIDACIÓN
-student_id = st.text_input("Ingresa tu cédula de identidad (sólo números)", max_chars=12).strip()
+raw_input = st.text_input("Ingresa tu cédula de identidad (sólo números)", max_chars=12)
+student_id = "".join(filter(str.isdigit, raw_input))
+
 if not student_id:
     st.warning("Ingrese su Cédula para comenzar.")
     st.stop()
@@ -21,9 +23,13 @@ random.seed(seed_val)
 ########################################################################################
 # 2. CONTENIDO (MODIFICAR AQUÍ)
 
-if 'is_admin' not in globals():
-    is_admin = False
-    
+area_visual = locals().get('sidebar_area', st.sidebar)
+
+# Python intenta entrar en el contexto del objeto
+with area_visual: 
+    st.write("Formulario")
+    # Colocar aquí una breve "cheat sheet" con la teoría necesaria para resolver el ejercicio.
+
 titulo = "Pregunta de Ejemplo"
 a = random.randint(1, 10)
 b = random.randint(1, 10)
@@ -32,21 +38,37 @@ solucion = a + b
 st.subheader(f"{titulo}",divider=True)
 st.info(f"¿Cuánto es {a} + {b}?")
 
-if is_admin:
-    st.warning(f"VISTA DOCENTE - Solución correcta: {solucion}") #Visualización para profesores
-
 respuesta = st.number_input("Respuesta:", step=0.01)
 
 ########################################################################################
 
-
 # 3. EVALUACIÓN
+
+def calcular_nota_personalizada(intentos_previos, total_aprobados): # NO MODIFICAR ESTA FUNCIÓN 
+    
+    nota_base = 20.0
+    inscritos = 25
+    penalizacion_por_intento = intentos_previos * 0.5    
+    penalizacion_por_ranking = total_aprobados * (10 / inscritos)
+    
+    nota_final = nota_base - penalizacion_por_intento - penalizacion_por_ranking    
+    
+    return max(nota_final, 10)
+
+
 if st.button("Enviar"):
     es_correcto = (respuesta == solucion)
-    intentos, nota = db.register_attempt(EXAM_ID, student_id, es_correcto)
+    
+    intentos, nota = db.register_attempt(
+        EXAM_ID, 
+        student_id, 
+        es_correcto, 
+        score_func=calcular_nota_personalizada
+    )
     
     if es_correcto:        
         st.success(f"¡Correcto! Nota: {nota}")
+        st.caption("Calculada de la siguiente manera: 20 - (penalización por fallos) - (factor competitivo)") 
     else:
         st.error("Incorrecto.")
 
